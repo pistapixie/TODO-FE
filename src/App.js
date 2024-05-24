77930bfdae539900bbcc577fda7228c6740436a4
pistapixie <hleehslee@gmail.com>
2024-05-25 08:44:49 +0900

    [Modify] TodoItem에 key prop 추가

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import TodoBoard from "./components/TodoBoard";
 
+import TodoBoard from "./components/TodoBoard";
+import api from "./utils/api";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import api from "./utils/api";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [todoValue, setTodoValue] = useState("");
+
   const getTasks = async () => {
     const response = await api.get("/tasks");
-    console.log("rrrr", response);
     setTodoList(response.data.data);
   };
-  const addTask = async () => {
+  useEffect(() => {
+    getTasks();
+  }, []);
+  const addTodo = async () => {
     try {
       const response = await api.post("/tasks", {
         task: todoValue,
         isComplete: false,
       });
       if (response.status === 200) {
-        console.log("성공");
-        //1.입력한 값이 안 사라짐
-        setTodoValue("");
-        //2. 추가한 값이 안 보임
         getTasks();
-      } else {
-        throw new Error("task can not be added");
       }
-    } catch (err) {}
+      setTodoValue("");
+    } catch (error) {
+      console.log("error:", error);
+    }
+  };
+
+  const deleteItem = async (id) => {
+    try {
+      console.log(id);
+      const response = await api.delete(`/tasks/${id}`);
+      if (response.status === 200) {
+        getTasks();
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   const toggleComplete = async (id) => {
@@ -47,10 +59,6 @@ function App() {
       console.log("error", error);
     }
   };
-
-  useEffect(() => {
-    getTasks();
-  }, []);
   return (
     <Container>
       <Row className="add-item-row">
@@ -58,19 +66,23 @@ function App() {
           <input
             type="text"
             placeholder="할일을 입력하세요"
+            onChange={(event) => setTodoValue(event.target.value)}
             className="input-box"
             value={todoValue}
-            onChange={(event) => setTodoValue(event.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add" onClick={addTask}>
+          <button onClick={addTodo} className="button-add">
             추가
           </button>
         </Col>
       </Row>
 
-      <TodoBoard todoList={todoList} toggleComplete={toggleComplete} />
+      <TodoBoard
+        todoList={todoList}
+        deleteItem={deleteItem}
+        toggleComplete={toggleComplete}
+      />
     </Container>
   );
 }
