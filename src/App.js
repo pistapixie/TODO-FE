310aafb4877fb8e33db062f4e7ccb5be18b9a9e2
pistapixie <hleehslee@gmail.com>
2024-05-25 02:19:08 +0900

    [Feat]할 일 추가 후 입력 필드 초기화 및 목록을 갱신하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect, useState } from "react";
 import TodoBoard from "./components/TodoBoard";
 
 import Row from "react-bootstrap/Row";
@@ -10,13 +10,29 @@ import api from "./utils/api";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-
+  const [todoValue, setTodoValue] = useState("");
   const getTasks = async () => {
     const response = await api.get("/tasks");
     console.log("rrrr", response);
     setTodoList(response.data.data);
   };
-
+  const addTask = async () => {
+    try {
+      const response = await api.post("/tasks", {
+        task: todoValue,
+        isComplete: false,
+      });
+      if (response.status === 200) {
+        console.log("성공");
+        //1.입력한 값이 안 사라짐
+        setTodoValue("");
+        //2. 추가한 값이 안 보임
+        getTasks();
+      } else {
+        throw new Error("task can not be added");
+      }
+    } catch (err) {}
+  };
   useEffect(() => {
     getTasks();
   }, []);
@@ -28,10 +44,14 @@ function App() {
             type="text"
             placeholder="할일을 입력하세요"
             className="input-box"
+            value={todoValue}
+            onChange={(event) => setTodoValue(event.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>
+            추가
+          </button>
         </Col>
       </Row>
 
